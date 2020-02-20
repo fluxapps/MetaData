ee51bc233e3ea36577c1d06baa4187fb5b3ba999
sragfw <fw@studer-raimann.ch>
2020-02-20 03:02:02 +0100

    Clone metadata on object clone

--- src/MetadataService.php
+++ src/MetadataService.php
@@ -7,6 +7,7 @@ use ilObject;
 use srag\DIC\MetaData\DICTrait;
 use SRAG\ILIAS\Plugins\MetaData\Field\Field;
 use SRAG\ILIAS\Plugins\MetaData\Field\FieldGroup;
+use SRAG\ILIAS\Plugins\MetaData\Field\NullField;
 use SRAG\ILIAS\Plugins\MetaData\Form\ilObjectMapping;
 use SRAG\ILIAS\Plugins\MetaData\Object\ConsumerObject;
 use SRAG\ILIAS\Plugins\MetaData\Object\ilConsumerObject;
@@ -83,6 +84,19 @@ class MetadataService
 
 
     /**
+     * @param int $field_group_id
+     *
+     * @return FieldGroup|null
+     */
+    public function getFieldGroupById(int $field_group_id)/*: ?FieldGroup*/
+    {
+        return FieldGroup::where([
+            "id" => $field_group_id
+        ])->first();
+    }
+
+
+    /**
      * @param string $field_group_identifier
      *
      * @return FieldGroup|null
@@ -90,6 +104,19 @@ class MetadataService
     public function getFieldGroupByIdentifier(string $field_group_identifier)/*: ?FieldGroup*/
     {
         return FieldGroup::findByIdentifier($field_group_identifier);
+    }
+
+
+    /**
+     * @param int $field_id
+     *
+     * @return Field|null
+     */
+    public function getFieldById(int $field_id)/*: ?Field*/
+    {
+        return NullField::where([
+            "id" => $field_id
+        ])->first();
     }
 
 
@@ -247,5 +274,44 @@ class MetadataService
         }
 
         return $field_groups;
+    }
+
+
+    /**
+     * @param ilObject $object
+     *
+     * @return Record[]
+     */
+    public function getRecordsForObject(ilObject $object) : array
+    {
+        return Record::where([
+            "obj_type" => $object->getType(),
+            "obj_id"   => $object->getId()
+        ])->get();
+    }
+
+
+    /**
+     * @param ilObject $old_obj
+     * @param ilObject $new_obj
+     */
+    public function cloneMetadata(ilObject $old_obj, ilObject $new_obj)/*:void*/
+    {
+        /**
+         * @var Record $record
+         */
+        foreach ($this->getRecordsForObject($old_obj) as $record) {
+            $group = $this->getFieldGroupById($record->getFieldGroupId());
+            if (!$group) {
+                continue;
+            }
+
+            $field = $this->getFieldById($record->getFieldId());
+            if (!$field) {
+                continue;
+            }
+
+            $this->setValue($new_obj, $group->getIdentifier(), $field->getIdentifier(), $this->getValue($old_obj, $group->getIdentifier(), $field->getIdentifier()));
+        };
     }
 }
