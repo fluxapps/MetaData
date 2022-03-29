795e60d42274dc661a0a10a3b666d29d3c4ef024
fluxms <martin@fluxlabs.ch>
2022-03-29 02:18:49 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = 'sragmetadata';
-$version = '1.1.0';
+$version = '1.2.0';
 $ilias_min_version = '6.99';
 $ilias_max_version = '7.99';
 $responsible_mail = 'support@fluxlabs.ch';
